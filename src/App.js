c71f2d162b2c1ec5cbdc2a67be72b041506339e5
selidrissi <s.elidrissi1992@gmail.com>
2025-02-15 00:37:40 +0100

    create Contact section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Skills from "./components/sections/Skills";
 import Experience from "./components/sections/Experience";
 import Certifications from "./components/sections/Certifications";
 import EarthCanvas from "./components/canvas/Earth";
+import Contact from "./components/sections/Contact";
+import StarCanvas from "./components/canvas/Stars";
+import React from "react";
 
 const Body = styled.div`
     background-color: ${({theme}) => theme.bg};
@@ -15,6 +18,21 @@ const Body = styled.div`
     overflow-x: hidden;
     position: relative;
 `
+const Wrapper = styled.div`
+    padding-bottom: 100px;
+    background: linear-gradient(
+            38.73deg,
+            rgba(204, 0, 187, 0.15) 0%,
+            rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+            141.27deg,
+            rgba(0, 70, 209, 0) 50%,
+            rgba(0, 70, 209, 0.15) 100%
+    );
+    width: 100%;
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
     return (
@@ -22,12 +40,21 @@ function App() {
             <BrowserRouter>
                 <Navbar/>
                 <Body>
-                    <Presentation/>
-                    <Skills/>
-                    <Experience/>
-                    <Certifications/>
-                    <br/>
-                    <EarthCanvas/>
+                    <StarCanvas/>
+                    <div>
+                        <Presentation/>
+                        <Wrapper>
+                            <Skills/>
+                            <Experience/>
+                        </Wrapper>
+
+                        <Certifications/>
+                        <br/>
+                        <Wrapper>
+                            <EarthCanvas/>
+                            <Contact/>
+                        </Wrapper>
+                    </div>
                 </Body>
             </BrowserRouter>
         </ThemeProvider>
