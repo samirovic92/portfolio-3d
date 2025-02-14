36be140717655abbcd848d826dcd514c7883d6a2
selidrissi <s.elidrissi1992@gmail.com>
2025-02-14 20:31:42 +0100

    Create Experience section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import {BrowserRouter} from "react-router-dom";
 import Presentation from "./components/sections/Presentation";
 import Skills from "./components/sections/Skills";
+import Experience from "./components/sections/Experience";
 
 const Body = styled.div`
     background-color: ${({theme}) => theme.bg};
@@ -19,8 +20,10 @@ function App() {
             <BrowserRouter>
                 <Navbar/>
                 <Body>
-                    <Presentation></Presentation>
-                    <Skills></Skills>
+                    <Presentation/>
+                    <Skills/>
+                    <Experience/>
+                    <Certifications />
                 </Body>
             </BrowserRouter>
         </ThemeProvider>
