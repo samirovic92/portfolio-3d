ad88616bca5391dc8e558e19ac0109f8c4fdf606
selidrissi <s.elidrissi1992@gmail.com>
2025-02-15 13:21:06 +0100

    Refacto

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -118,9 +118,7 @@ const Contact = () => {
                 process.env.EMAIL_JS_SERVICE_ID,
                 process.env.EMAIL_JS_TEMPLATE_ID,
                 form.current,
-                {
-                    publicKey: process.env.EMAIL_JS_PUBLIC_KEY
-                }
+                {publicKey: process.env.EMAIL_JS_PUBLIC_KEY}
             )
             .then(
                 () => alert("Message Sent successfully"),
